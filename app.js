f4112ebcf2751df427d63904af037d4cabe278ce
jasonc-dev <jasoncohen07@gmail.com>
2021-05-11 22:30:05 +0100

    implemented cors and other minor changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.locals.basedir = path.join(__dirname, 'views');
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+app.options('*', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
